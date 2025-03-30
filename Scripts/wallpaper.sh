2075e51cb25816c20fe30f12d735be41f24d3c11
Mithun Sivanesan <mithunsivanesan@gmail.com>
2025-03-30 14:57:06 +1100

    Replaced hyprpaper and dunst with swww and swaync

--- Scripts/wallpaper.sh
+++ Scripts/wallpaper.sh
@@ -2,18 +2,15 @@
 
 # Directory containing wallpapers
 WALLPAPER_DIR="$HOME/Pictures/Wallpapers"
-HYPRPAPER="$HOME/.config/hypr/hyprpaper.conf"
+HYPRLAND="$HOME/.config/hypr/hyprland.conf"
 WALLPAPERS=$(ls $WALLPAPER_DIR)
 
-#WALLPAPER=$(printf "$ROFI_STR"| rofi -dmenu -i -p "Wallpaper")
 WALLPAPER=$(for a in $WALLPAPER_DIR/*.png; do echo -en "$(basename "$a")\0icon\x1f$a\n" ; done | rofi -config $HOME/.local/share/rofi/themes/cat_wp.rasi -dmenu)
 if [ -z "$WALLPAPER" ]; then
     exit 0
 else
     wp="$WALLPAPER_DIR/$WALLPAPER"
-    hyprctl hyprpaper reload ,"$wp"
-    sed -i -e "s#preload=.*#preload=$wp#g" "$HYPRPAPER"
-    sed -i -e "s#wallpaper=,.*#wallpaper=,$wp#g" "$HYPRPAPER" 
+    swww img "$wp" --transition-type=grow --transition-duration=1.5 --transition-pos=0.89,0.975  
+    sed -i -e "s#swww img .*#swww img $wp#g" "$HYPRLAND"
 fi
 
-# Set the wallpaper using hyprpaper
